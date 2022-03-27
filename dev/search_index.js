d8db68d3cf827ace20611119a83f0369a977bd9d
Documenter.jl <documenter@juliadocs.github.io>
2022-03-27 05:40:07 +0000

    build based on c9f735f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sunaasobi","category":"page"},{"location":"#Sunaasobi","page":"Home","title":"Sunaasobi","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Sunaasobi.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sunaasobi]","category":"page"}]
+}
